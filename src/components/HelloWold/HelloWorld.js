64cd6175414330d06a21ae09afcfa1a958ff7dad
Arthue Debruille <arthur.debruille@esadd.fr>
2022-03-22 21:27:28 +0100

    Global Changes

--- src/components/HelloWold/HelloWorld.js
+++ src/components/HelloWold/HelloWorld.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./style/HelloWorld.css";
+import Img from "../../assets/img/adeb.png";
 
 export default class HelloWorld extends Component {
   render() {
     return (
-      <div className="welcome absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
-        <h1>HelloWorld</h1>
+      <div>
+        <img src={Img} alt="adeb profil" className="m-auto mt-[100px]" />
+        <h1 className="welcome absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
+          HelloWorld
+        </h1>
       </div>
     );
   }
 }
-
