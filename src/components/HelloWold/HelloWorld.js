24f51f955769f938551b2d6fc9c0046cf4297fa2
ADEB <elliurbedruhtra@gmail.com>
2022-03-23 22:24:40 +0100

    img css changes

--- src/components/HelloWold/HelloWorld.js
+++ src/components/HelloWold/HelloWorld.js
@@ -6,7 +6,7 @@ export default class HelloWorld extends Component {
   render() {
     return (
       <div>
-        <img src={Img} alt="adeb profil" className="m-auto mt-[100px]" />
+        <img src={Img} alt="adeb profil" className="absolute top-[25%] left-[50%] translate-y-[-50%] translate-x-[-50%]" />
         <h1 className="welcome absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
           HelloWorld
         </h1>
